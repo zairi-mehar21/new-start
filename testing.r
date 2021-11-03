23a658edd83e1075587f6c9f2a3342e16cc289a4
Uzair <uzair_dost21@yahoo.com>
2021-11-04 03:21:32 +0500

    Today's working.

--- testing.r
+++ testing.r
@@ -1,9 +1,35 @@
-# From Github
-print("Hello from Github")
+# Creating a Matrix 
 
-name = "uzair"
-version = "RStudio"
+m <- matrix(1:6, nrow = 2, ncol = 3)
+m
+# dimension
+dim(m)
+#attributes
+attributes(m)
+#.....................................
 
-print(name)
-print("These lines has been written in RStudio.")
+#creating a vector and then convert into matrix
+m <- 1:10
+m
+dim(m) <- c(2,5)
+m
+#...............................................
 
+#Binding rows and columns
+x <- 1:5
+y <- 10:14
+
+rbind(x, y)
+cbind(x, y)
+#................................
+
+# list are used to store different types(classes) of values.
+# yaha "12" aik character hai jis ki waja se corecion apply ho jati hai or sari values
+# characters ban jati hai.. is se bachne k liye hum "list" use kare gy.
+v <- c(1,TRUE, "12", 4)
+v
+v <- list(1,TRUE, "12", 4)
+v
+#........................................................................................
+
+# factors hum tab use karty hain data ki darja bandi hui ho.
